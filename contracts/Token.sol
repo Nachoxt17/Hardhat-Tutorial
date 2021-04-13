d119cfc03a30cf9dab37d4bfb6dfe3938e03b230
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-04-13 19:53:31 -0300

    Test changes

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -41,7 +41,7 @@ contract Token {
      */
     function transfer(address _to, uint256 _amount) external {
         console.log("Sender balance is %s tokens", balances[msg.sender]);
-        console.log("Trying to send %s tokens to %s", amount, to);
+        console.log("Trying to send %s tokens to %s", _amount, _to);
 
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,22 +4,22 @@ require("@nomiclabs/hardhat-waffle");
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "KEY";
+//---const ALCHEMY_API_KEY = "KEY";
 
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+//---const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
 
 module.exports = {
   solidity: "0.7.3",
-  networks: {
+  /*networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
     }
-  }
+  }**/
 };
 
 /**+-We're using Alchemy( https://www.alchemyapi.io/ ), but pointing url to any Ethereum node or gateway would work. Go grab your ALCHEMY_API_KEY 
